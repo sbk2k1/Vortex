d8a46f32ef1d83f6d5d09c2d44b94f928f98f40c
sbk2k1 <bhattacharyasaptarshi2001@gmail.com>
2021-12-01 19:10:14 +0530

    minor adjustment

--- src/models/user.js
+++ src/models/user.js
@@ -98,6 +98,7 @@ userSchema.statics.findByCredentials = async (email, password) => {
     return user
 }
 
+//NEVER DO IT LIKE THIS. HANDLE THIS IN THE ROUT
 // Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
     const user = this
